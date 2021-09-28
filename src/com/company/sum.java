8788216fc761bde6d9d3ce145354ec390c4fc39b
Roman Tataryn <rtataryn@gmail.com>
2021-09-28 13:43:40 +0300

    Add files via upload

--- src/com/company/sum.java
+++ src/com/company/sum.java
@@ -5,14 +5,15 @@ public class sum {
         double array2[] = {-2.5, 10.01, -7.051, 44.1, 1057};
         Suma(array2);
     }
-    public static double Suma (double[] array2) {
+
+    public static double Suma(double[] array2) {
         double Sum = 0.0;
         for (int i = 0; i < array2.length; i++) {
             if (array2[i] > 0) {
                 Sum += array2[i];
             }
         }
-        System.out.println("сума додатніх чисел масиву: " +Sum);
+        System.out.println("сума додатніх чисел масиву: " + Sum);
         return Sum;
 
     }
