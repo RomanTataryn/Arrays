9faca624d0c50af1aa3d68adc6b3a88369956a31
Roman Tataryn <rtataryn@gmail.com>
2021-09-29 19:25:02 +0300

    Add files via upload

--- src/com/company/sort.java
+++ src/com/company/sort.java
@@ -3,30 +3,31 @@ package com.company;
 public class sort {
     public static void main(String[] args) {
         int[] array = {2, 3, 1, 7, 11};
-       // System.out.print();
+        // System.out.print();
         Bubblesort(array);  // Метод сортування бульбашкою
 
     }
 
-        public static void Bubblesort(int[] array) {
+    public static void Bubblesort(int[] array) {
 
-            boolean yezamina = true; int k=0;
-            while (yezamina) {
-                yezamina = false;
-                for (int i = 0; i < array.length - 1-k; i++) {
+        boolean yezamina = true;
+        int k = 0;
+        while (yezamina) {
+            yezamina = false;
+            for (int i = 0; i < array.length - 1 - k; i++) {
 
-                    if (array[i] < array[i + 1]) {
-                        int temp = array[i];
-                        array[i] = array[i + 1];
-                        array[i+1] = temp;
-                        yezamina = true;
-                    }
+                if (array[i] < array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    yezamina = true;
                 }
-                k++;
             }
-            for (int i : array)
-                System.out.print(i+" ");
-
+            k++;
         }
+        for (int i : array)
+            System.out.print(i + " ");
+
+    }
 
 }
