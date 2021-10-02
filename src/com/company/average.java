f199e92a40c8f59de0c1cb0d8fa97e711522c3e6
Roman Tataryn <rtataryn@gmail.com>
2021-10-02 18:12:52 +0300

    Update average.java

--- src/com/company/average.java
+++ src/com/company/average.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class average {
 
     public static void main(String[] args) {
-        int Array[] = new int[5];
+        int array[] = new int[5];
         int k = 0;                // k-кількість елементів масиву
-        double Sum = 0, SA = 0;   // Sum- сума елементів масиву, SA- середнє арифметичне
-        Scanner Scan = new Scanner(System.in);
+        double sum = 0, sa = 0;   // sum- сума елементів масиву, sa- середнє арифметичне
+        Scanner scan = new Scanner(System.in);
         System.out.println("введіть 5 елементів масиву");
-        for (int i = 0; i < Array.length; i++) {
-            Array[i] = Scan.nextInt();
-            Sum += Array[i];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scan.nextInt();
+            sum += array[i];
             k++;
         }
-        SA = Sum / k;
+        sa = sum / k;
 
-        System.out.println("сума чисел: " + Sum);
+        System.out.println("сума чисел: " + sum);
         System.out.println("кількість елементів масиву: " + k);
-        System.out.println("середнє арифметичне введеного масиву чисел: " + SA);
+        System.out.println("середнє арифметичне введеного масиву чисел: " + sa);
 
 
     }
